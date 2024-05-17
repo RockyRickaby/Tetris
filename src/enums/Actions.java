1dc64d53bd1ff45eab432c6a6a9b3507ac186b7e
mauricioluz22 <mauricio.luz22@edu.pucrs.br>
2024-05-17 10:30:01 -0300

    added enums. I may add more enums in the future if necessary

---
+++ src/enums/Actions.java
@@ -0,0 +1,10 @@
+package enums;
+
+public enum Actions {
+    MOVE_LEFT,
+    MOVE_RIGHT,
+    MOVE_DOWN,
+    HARD_DROP,
+    ROTATE_CLOCKWISE,
+    ROTATE_COUNTERCLOCKWISE;
+}
---
+++ src/enums/State.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum State {
+    MAIN_MENU,
+    IN_GAME,
+    GAME_OVER;
+}
